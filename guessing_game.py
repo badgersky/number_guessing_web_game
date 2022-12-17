ad4b7ea7e5e0a9ce394f35902e0603f25742e33c
badgersky <szymonek.ptasznik@gmail.com>
2022-12-17 11:38:30 +0100

    added title to html

--- guessing_game.py
+++ guessing_game.py
@@ -5,19 +5,19 @@ from random import randint
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST', 'GET'])
-def guessing_game():
+@app.route('/', methods=['GET', 'POST'])
+def game_loop():
     if request.method == 'POST':
-        p_answer = request.form['answer']
-        pc_number = int(request.form['guess'])
         mi_num = int(request.form['min'])
         mx_num = int(request.form['max'])
+        pc_number = int(request.form['guess'])
+        p_answer = request.form['answer']
         if p_answer == 'too big':
             mx_num = pc_number
         elif p_answer == 'too small':
             mi_num = pc_number
         else:
-            msg = f'I have won!'
+            msg = 'I have won!'
             return msg
 
         pc_number = randint(mi_num, mx_num)
